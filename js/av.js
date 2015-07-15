41ef45feadc6eee22e30f99e7c76ff03ba180f97
Nick Salloum <nick@callmenick.com>
2015-07-14 20:11:37 -0700

    first try on bonus features

--- js/av.js
+++ js/av.js
@@ -3,6 +3,8 @@
 // Cache some variables
 var numParticles = 256;
 var colours = ['#ff0046', '#ffd700', '#99ff00', '#00ccff'];
+var nodes = [];
+var f = 10;
 
 // Cross browser audio context
 var AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -23,11 +25,22 @@ audio.addEventListener('canplay', function() {
   var source = context.createMediaElementSource(audio);
   source.connect(analyser);
   analyser.connect(context.destination);
+
+  // UNCOMMENT BELOW FOR PRODUCTION
+  // ++++++++++++++++++++++++++++++
   setTimeout(function() {
     document.querySelector('#init').classList.add('init--hidden');
     audio.play();
     d3.timer(pulse);
+    initGestures();
   }, 2000);
+  
+  // COMMENT BELOW FOR PRODUCTION
+  // ++++++++++++++++++++++++++++
+  // document.querySelector('#init').classList.add('init--hidden');
+  // audio.play();
+  // d3.timer(pulse);
+  // initGestures();
 });
 
 // Set up visualization stage
@@ -38,12 +51,14 @@ for (var i = 0; i < numParticles; i++) {
   var cx = Math.random() * 100 + '%';
   var cy = Math.random() * 100 + '%';
 
-  v.append('circle')
+  var node = v.append('circle')
    .attr('class', 'node')
    .attr('cx', cx)
    .attr('cy', cy)
    .attr('r', 2)
    .attr('fill', colours[i % 4]);
+
+  nodes.push(node);
 }
 
 // This is the pulse callback that gets passed into the d3 timer
@@ -54,6 +69,19 @@ var pulse = function() {
    .style('transform', function(d) {
      return 'scale(' + Math.abs(d)/10 + ')';
    });
+};
+
+// Gestures for mouse movement
+var initGestures = function() {
+  d3.select('body').on('mousemove', function(e) {
+    var mouseX = d3.event.x;
+    var mouseY = d3.event.y;
+
+    for (var i = 0; i < nodes.length; i++) {
+      var node = nodes[i];
+      
+    }
+  });
 };
 
 })();
